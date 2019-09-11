6b83922282369b1de813afe99bced23d3ddf79db
Andrew Waller <andrewhwaller@gmail.com>
2019-09-11 22:44:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,25 +3,25 @@ class BoardMember {
     this.name = name;
     this.homeState = homeState;
     this.training = training;
-  }
+  };
 
   veto() {
     return 'No, I must disagree'
-  }
+  };
 
   approve() {
     return 'You can do that!'
-  }
+  };
 
   doCharity() {
     return 'I like to help people.'
-  }
+  };
 
   releasePressStatement() {
     return 'You will see great things from Scuber.'
-  }
+  };
 
   sayHi() {
     return `Hi, my name is ${name}. I am from ${homeState}, and I was trained in ${training}.`
-  }
-}
+  };
+};
